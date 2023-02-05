652d5ac30f50b7f52ae762df0d26ed49c1db65dd
Anoj Kumar <anoj@pushowl.com>
2023-02-05 17:26:20 +0530

    celery is configured

--- push_notification/apps/webpush/services/webpush_service.py
+++ push_notification/apps/webpush/services/webpush_service.py
@@ -3,6 +3,7 @@ from pywebpush import WebPushException
 from apps.subscriber.api_services.subscriber import SubscriberAPIService
 from apps.notification.api_services.notification import NotificationAPIService
 from ..utils.webpush_utils import WebPushRequestUtils
+from ..tasks.webpush_task import task_send_notification
 
 logger = logging.getLogger(__name__)
 
@@ -28,30 +29,4 @@ class WebPushService(object):
         subscriber_ids = cls.get_subscriber_ids(subscribers)
         logger.info("subscriber-ids: [{}]".format(subscriber_ids))
         for subscriber_id in subscriber_ids:
-            cls.task_send_push_notification(subscriber_id, notification_data)
-
-    @classmethod
-    def task_send_push_notification(cls, subscriber_id, notification_data):
-        WebPushService().send_notification(subscriber_id, notification_data)
-
-    @classmethod
-    def send_notification(cls, subscription_id, notification_data):
-        subscriber_data = SubscriberAPIService().get_subscriber_subscription_data(subscription_id)
-        logger.info(
-            msg="Creating webpush request for subscriber_id={} with notification_data {}"
-            .format(subscription_id, notification_data)
-        )
-        try:
-            WebPushRequestUtils().send(
-                subscription_data=subscriber_data,
-                notification_data=notification_data
-            )
-            logger.info(
-                msg="Successfully send push notification for subscriber_id={} with notification_data {}"
-                .format(subscription_id, notification_data)
-            )
-        except WebPushException as e:
-            logger.info(
-                msg="Failed to send push notification, exception={} for subscriber_id={} with notification_data {}"
-                .format(e, subscription_id, notification_data)
-            )
+            task_send_notification.delay(subscriber_id, notification_data)
---
+++ push_notification/apps/webpush/tasks/webpush_task.py
@@ -0,0 +1,30 @@
+import logging
+from celery import shared_task
+from pywebpush import WebPushException
+from apps.subscriber.api_services.subscriber import SubscriberAPIService
+from ..utils.webpush_utils import WebPushRequestUtils
+
+logger = logging.getLogger(__name__)
+
+
+@shared_task()
+def task_send_notification(subscription_id, notification_data):
+    subscriber_data = SubscriberAPIService().get_subscriber_subscription_data(subscription_id)
+    logger.info(
+        msg="Creating webpush request for subscriber_id={} with notification_data {}"
+        .format(subscription_id, notification_data)
+    )
+    try:
+        WebPushRequestUtils().send(
+            subscription_data=subscriber_data,
+            notification_data=notification_data
+        )
+        logger.info(
+            msg="Successfully send push notification for subscriber_id={} with notification_data {}"
+            .format(subscription_id, notification_data)
+        )
+    except WebPushException as e:
+        logger.info(
+            msg="Failed to send push notification, exception={} for subscriber_id={} with notification_data {}"
+            .format(e, subscription_id, notification_data)
+        )
