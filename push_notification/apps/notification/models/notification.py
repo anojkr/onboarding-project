99fcdfa049a4ce6902136a15734449082e6d6f5c
Anoj Kumar <122958847+anoj-pushowl@users.noreply.github.com>
2023-02-06 14:34:05 +0530

    Notification App with crud API (#2)
    * 1. Created and configured push_notification project
    2. Created subsciber app with respective crud api
    3. Configured postgres
    
    * RefactorCode: As per suggested module structure
    
    * TestCases Added for API and APIService
    
    * Model fields size increased
    
    * Added Notification-App

---
+++ push_notification/apps/notification/models/notification.py
@@ -0,0 +1,16 @@
+from django.db import models
+from apps.common.models.base_entity import DateTimeEntity
+
+
+class Notification(DateTimeEntity):
+    title = models.CharField("Title", null=False, blank=False, max_length=100)
+    description = models.CharField("Description", null=False, blank=True, max_length=500)
+
+    class Meta:
+        unique_together = (
+            "title",
+            "description",
+        )
+        verbose_name = "notification"
+        verbose_name_plural = "notifications"
+        ordering = ("-created_at",)
--- push_notification/push_notification/settings.py
+++ push_notification/push_notification/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django_extensions',
     'rest_framework',
     'apps.subscriber',
+    'apps.notification',
     'corsheaders'
 ]
 
