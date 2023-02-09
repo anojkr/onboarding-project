13ff4ec0b2f84eb763fff6979e9c2f53ee5e4f8a
Anoj Kumar <anoj@pushowl.com>
2023-02-09 16:09:02 +0530

    show-notification-list

--- push_notification_ui/static/js/main.js
+++ push_notification_ui/static/js/main.js
@@ -1,8 +1,13 @@
 import {grantingNotificationPermission} from "./register-subscriber.js"
 import {createNotification} from "./requests.js"
+import {getNotificationItems} from "./notifications.js"
 
 const subscribe_notification_btn = document.getElementById("subscriber_notification_btn")
 const notification_form = document.getElementById('notification-form')
+const reload_notification = document.getElementById('refresh-notifications-btn')
+const notification_items = document.getElementById('notifications-items')
+
+getNotificationItems(notification_items)
 
 subscribe_notification_btn.addEventListener('click', function(event) {
     event.preventDefault()
@@ -14,3 +19,7 @@ notification_form.addEventListener('submit', function(event) {
     createNotification(notification_form)
 });
 
+reload_notification.addEventListener('click', function(event) {
+    event.preventDefault()
+    getNotificationItems(notification_items)
+})
